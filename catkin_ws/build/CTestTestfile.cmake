d3e2f161c974e63d1a2d0f8ec930367a6b42bd04
Mary Robertson <maryrobertson1043@gmail.com>
2024-07-21 17:24:05 -0700

    init

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jasper123/catkin_ws/src
+# Build directory: /home/jasper123/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mobile_manipulator_body")
---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/jasper123/catkin_ws/src'
+whitelisted_packages = 'mobile_manipulator_body'.split(';') if 'mobile_manipulator_body' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
