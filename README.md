6ce181b85ae4913e035f9993726320c6a07cfdc9
x-dl <2567434944@qq.com>
2024-07-25 03:06:38 -0700

    need to solve rosdep update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # workspace
-we need to change something.
+the link for tutorials resource is https://husarion.com/tutorials/ros-tutorials/1-ros-introduction/
+maybe rosdep update is a big questions for us .
+
--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH='/home/jasper123/workspace/catkin_ws/devel:/opt/ros/noetic'
-export LD_LIBRARY_PATH='/opt/ros/noetic/lib'
-export PKG_CONFIG_PATH='/opt/ros/noetic/lib/pkgconfig'
 export PWD='/home/jasper123/workspace/catkin_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/jasper123/workspace/catkin_ws/devel/share/common-lisp'
-export ROS_PACKAGE_PATH='/home/jasper123/workspace/catkin_ws/src:/opt/ros/noetic/share'
